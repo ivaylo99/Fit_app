34b04f76de75243ec9bbb73f3e36fbbd90cd64ad
Ivaylo <ivaylo.vasilevtues@gmail.com>
2017-12-15 23:26:41 +0200

    Navigation drawer update
    change activity on navigation item click

--- app/src/main/java/com/example/ivo/fit_app/User_Area_Activity1.java
+++ app/src/main/java/com/example/ivo/fit_app/User_Area_Activity1.java
@@ -1,20 +1,25 @@
 package com.example.ivo.fit_app;
 
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toolbar;
 
 public class User_Area_Activity1 extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_area_1);
+
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
         mToggle = new ActionBarDrawerToggle(this , mDrawerLayout , R.string.open , R.string.close);
@@ -23,6 +28,19 @@ public class User_Area_Activity1 extends AppCompatActivity {
         mToggle.syncState();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView mNavigationView = (NavigationView) findViewById(R.id.nav_menu);
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem){
+                switch (menuItem.getItemId()){
+                    case(R.id.nav_account):
+                        Intent accountActivity = new Intent(getApplicationContext(), Login_Activity.class);
+                        startActivity(accountActivity);
+                }
+                return true;
+            }
+        });
     }
 
     @Override
